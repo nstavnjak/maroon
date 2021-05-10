706e8824c75564fc6cdcc46653c8e91cd7d4ebaf
Elaskuss <ivan.k@live.se>
2021-05-10 16:25:59 +0200

    Enter

--- script/education.js
+++ script/education.js
@@ -40,11 +40,7 @@ function createOPT(){
     let emptyOPT = document.createElement("option");
     document.querySelector("#select1").append(emptyOPT);
     document.querySelector("#select2").append(emptyOPT);
-    COUNTRIES.forEach(e => {
-        let option = document.createElement("option");
-        option.innerHTML = e.name;
-        document.querySelector("#select1").append(option);
-    });
+    
     
     if (document.querySelector("select1").value === true){
         let countrySerch = document.querySelector("select1").value;

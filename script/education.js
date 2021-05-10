c10fd7d44e836264cd032efecf62b2f32da8a9ff
Elaskuss <ivan.k@live.se>
2021-05-10 16:49:15 +0200

    Enter

--- script/education.js
+++ script/education.js
@@ -49,7 +49,7 @@ function createOPT(){
         document.querySelector("#select1").append(option);
     });
     
-    document.querySelector("#select1").addEventListener("onchange", function(){
+    document.querySelector("#select1").addEventListener("change", function(){
         if(document.querySelector("#select1").value = true){
             console.log("hej");
         }

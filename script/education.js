81a3acf14dfc695b03eb33b4bf87957637f8de65
Elaskuss <ivan.k@live.se>
2021-05-10 16:13:12 +0200

    Enter

--- script/education.js
+++ script/education.js
@@ -1,8 +1,7 @@
 "use strict";
 
 // Head
-appendLink("stylesheets/education.css");
-appendLink("stylesheets/common_style.css");
+
 
 // Meny
 
@@ -11,16 +10,44 @@ appendLink("stylesheets/common_style.css");
 
 
 // Footer 
-createFooter();
+
 
 // Functions
+createSearchForm();
 
 function createSearchForm(){
+    let searchForm = document.createElement("div");
+    searchForm.setAttribute("id", "searchForm");
 
+    let inputField = document.createElement("input");
+    inputField.setAttribute("id", "inputField");
+
+    let select1 = document.createElement("select");
+    select1.setAttribute("id", "select1");
+
+    let select2 = document.createElement("select");
+    select2.setAttribute("id", "select2");
+
+    let search = document.createElement("button");
+    search.setAttribute("id", "search");
+
+    document.querySelector("body").append(searchForm, inputField, select1, select2, search);
 }
 
-function createOPT(){
 
+function createOPT(){
+    DB.COUNTRIES.forEach(e => {
+        let option = document.createElement("option");
+        option.innerHTML = e.name;
+        document.querySelector("select1").append(option);
+    });
+    
+    if (document.querySelector("select1").value === true){
+        let countrySerch = document.querySelector("select1").value;
+        DB.COUNTRIES.find(e => {
+            e.name === document.querySelector("select1").value;
+        });
+    }
 }
 
 function updateCityByCountry(){

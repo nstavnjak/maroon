8de9102e431360f014c58e9602cb771cfdf639c4
Elaskuss <ivan.k@live.se>
2021-05-10 15:20:43 +0200

    Enter

--- script/education.js
+++ script/education.js
@@ -13,58 +13,55 @@
 
 
 // Functions
-console.log("hej");
 
+function createSearchForm(){
+    let searchForm = document.createElement("div");
+    searchForm.setAttribute(‘id’, searchForm’);
 
-// function createSearchForm(){
-//     let searchForm = document.createElement("div");
-//     searchForm.setAttribute(‘id’, searchForm’);
+    let inputField = document.createElement("input");
+    inputField.setAttribute(‘id’, inputField);
 
-//     let inputField = document.createElement("input");
-//     inputField.setAttribute(‘id’, inputField);
+    let select1 = document.createElement("select");
+    select1.setAttribute(‘id’, select1);
 
-//     let select1 = document.createElement("select");
-//     select1.setAttribute(‘id’, select1);
+    let select2 = document.createElement("select");
+    select2.setAttribute(‘id’, select2);
 
-//     let select2 = document.createElement("select");
-//     select2.setAttribute(‘id’, select2);
+    let search = document.createElement("button");
+    search.setAttribute(‘id’, search);
+}
 
-//     let search = document.createElement("button");
-//     search.setAttribute(‘id’, search);
-// }
+function createOPT(){
 
-// function createOPT(){
+}
 
-// }
+function updateCityByCountry(){
 
-// function updateCityByCountry(){
+}
 
-// }
+function createSortAlternative(){
 
-// function createSortAlternative(){
+}
 
-// }
+function sortProgram(){
 
-// function sortProgram(){
+}
 
-// }
-
-// function createCard(){
+function createCard(){
     
 
-//     function createFront(){
+    function createFront(){
 
-//         function getProgrameSeats(){
+        function getProgrameSeats(){
 
-//         }
+        }
 
-//         function getProgrameGraduating(){
+        function getProgrameGraduating(){
 
-//         }
+        }
 
-//         function getProgrameEntrypoints(){
+        function getProgrameEntrypoints(){
 
-//         }
-//     }
-// }
-// */
+        }
+    }
+}

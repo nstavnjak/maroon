5f1b53d8738aae2bc2d1254421d3bafffb684768
nstavnjak <niklasstavnjak@gmail.com>
2021-05-10 14:18:46 +0200

    new

---
+++ script/common_script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// Head
+
+
+// Meny
+
+
+// Main
+
+
+// Footer 
+
+
+// Functions
+
+function appendLink(link){
+
+}
+
+function meny(){
+
+}
+
+function filter(){
+
+}
+
+function loadMore(){
+
+}
+
+function createBack(cityobject){
+
+    function getCityInfo(cityobject){
+        
+    }
+
+    function getCityRatingNightlife(){
+
+    }
+
+    function getCityRatingFood(){
+        
+    }
+
+    function getCityRatingHousing(){
+        
+    }
+
+    function createStudentsComments(){
+
+    }
+
+}

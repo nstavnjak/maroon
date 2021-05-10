d0925ba0055508d04be263e786e3480dd0a78e63
Mehmet-Kaan <taspunar10@hotmail.com>
2021-05-10 16:24:33 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 // Head
-appendLink("../stylesheets/index.css");
-appendLink("../stylesheets/common_style.css");
+appendLink("stylesheets/common_style.css");
+appendLink("stylesheets/index.css")
 
 // Meny
 

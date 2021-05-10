abc8b95ee13141c920dafd3f55b9f06ef23ec3a7
Elaskuss <ivan.k@live.se>
2021-05-10 15:12:21 +0200

    Enter

--- script/education.js
+++ script/education.js
@@ -13,9 +13,23 @@
 
 
 // Functions
+console.log("hej");
 
 function createSearchForm(){
+    let searchForm = document.createElement("div");
+    searchForm.setAttribute(‘id’, searchForm’);
 
+    let inputField = document.createElement("input");
+    inputField.setAttribute(‘id’, inputField);
+
+    let select1 = document.createElement("select");
+    select1.setAttribute(‘id’, select1);
+
+    let select2 = document.createElement("select");
+    select2.setAttribute(‘id’, select2);
+
+    let search = document.createElement("button");
+    search.setAttribute(‘id’, search);
 }
 
 function createOPT(){

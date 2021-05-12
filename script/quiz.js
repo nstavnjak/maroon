f605e88e1d8b031ef3d4d2d58c5a26d8998757c7
Mehmet-Kaan <taspunar10@hotmail.com>
2021-05-12 11:56:18 +0200

    createdProgressBar function

--- script/quiz.js
+++ script/quiz.js
@@ -4,11 +4,67 @@
 appendLink("stylesheets/quiz.css");
 
 // Meny
-
+document.body.append(createProgressBar());
 
 // Main
 
+const questions = [
+    {
+        "id":1,
+        "question": "1) "
+    },
+    {
+        "id":2,
+        "question": "2) "
+    },
+    {
+        "id":3,
+        "question": "3)"
+    },
+    {
+        "id":4,
+        "question": "4)"
+    },
+    {
+        "id":5,
+        "question": "5)"
+    },
+    {
+        "id":6,
+        "question": "6)"
+    },
+    {
+        "id":7,
+        "question": "7)"
+    },
+    {
+        "id":8,
+        "question": "8)"
+    },
+    {
+        "id":9,
+        "question": "9)"
+    },
+    {
+        "id":10,
+        "question": "10)"
+    },
+ ];
 
 // Footer 
 
 // Functions
+
+function createProgressBar(){
+    let progress = document.createElement("div");
+    progress.setAttribute("id", "Progress");
+
+    let bar = document.createElement("div");
+    bar.setAttribute("id", "bar");
+
+    progress.append(bar);
+
+    return progress;
+}
+
+console.log(questions);

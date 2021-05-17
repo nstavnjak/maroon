89f302818c6168e3fa44035e03ee1d63485def68
nstavnjak <niklasstavnjak@gmail.com>
2021-05-17 18:03:13 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -6,8 +6,30 @@ appendLink("../stylesheets/index.css")
 
 
 // Main
-
+document.querySelector("main").append(topPlace());
 
 // Footer 
 
 // Functions
+
+function topPlace(){
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("firstPageWrapper");
+    
+    let callForAction = document.createElement("div");
+    callForAction.classList.add("callForAction");
+
+    let circleAction = document.createElement("div"); 
+    circleAction.classList.add("circleAction");
+
+
+}
+
+function createGuideParts(title, text, img){
+    let wrapper = document.createElement("div");
+    wrapper.classList.add("guidePart");
+
+    let textBox = document.createElement("div");
+
+    let pictureBox = document.createElement("img");
+}

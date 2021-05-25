925aec3388abae513bb7036cfafbbc9c928750e5
Elaskuss <ivan.k@live.se>
2021-05-25 12:36:37 +0200

    Update education.js

--- script/education.js
+++ script/education.js
@@ -241,104 +241,75 @@ document.addEventListener("click", filter);
 
 function searchProgram(textValue, country, city){
     programlist.innerHTML = "";
-    finishArray = [];
-
+    finishArrayFiltered = [];
+    console.log(country);
     // Om allt är tomt så ska vi visa alla program i bokstavsordning från A-Ö - done
-    if (textValue.length === 0 && country.length === 0 && city.length === 0){
-        finishArray = PROGRAMMES;
-        finishArray.sort((a, b) => a.name > b.name) ? -1 : 1;
-        appendCards(finishArray);
+    if (country == "--- Land ---" && city === "--- Stad ---"){
+        if(textValue.length === 0){
+            finishArrayFiltered = PROGRAMMES;
+            finishArrayFiltered.sort((a, b) => a.name > b.name) ? -1 : 1;
+            appendCards(finishArrayFiltered);
+        }
+        else{
+            finishArrayFiltered = PROGRAMMES.filter(prog => prog.name.toLowerCase().includes(textValue));
+            appendCards(finishArrayFiltered);
+        }
     }
-    // Om bara land är valt - done
-    else if (textValue.length === 0 && country.length > 0 && city.length === 0){
-        let countryObj = COUNTRIES.find(c => c.name.includes(country));
-        let citys = CITIES.filter(ci => ci.countryID === countryObj.id);
-    
-        let universities = [];
-        citys.forEach(city => {
-            universities.push(UNIVERSITIES.filter(uni => uni.cityID === city.id));
-        })
-
-        universities.flat(1).forEach(uni => {
-            finishArray.push(PROGRAMMES.filter(prog => prog.universityID === uni.id));
-        })
-
-        // mergar ihop nestlade arrayer
-        finishArray = finishArray.flat(1);
-        appendCards(finishArray);
-    } 
-    // Om bara stad är valt - done
-    else if (textValue.length === 0 && country.length === 0 && city.length > 0){
+    // Om stad eller stad och land och/eller input är ifyllt - done
+    else if (city != "--- Stad ---"){
         let cityObj = CITIES.find(c => c.name.includes(city));
         let universities = UNIVERSITIES.filter(uni => uni.cityID === cityObj.id);
 
-        universities.forEach(uni => {
-            finishArray.push(PROGRAMMES.filter(prog => prog.universityID === uni.id))
-        })
-        
-        finishArray = finishArray.flat(1);
-        appendCards(finishArray);
-    } 
-    // Om bara sökfältet är är ifyllt - done
-    else if (textValue.length > 0 && country.length === 0 && city.length === 0){
-        finishArray = PROGRAMMES.filter(prog => prog.name.toLowerCase().includes(textValue));
-        appendCards(finishArray);
-    }
-    // Om stad och land är ifyllt - done
-    else if (textValue.length === 0 && country.length > 0 && city.length > 0){
-        let cityObj = CITIES.find(c => c.name.includes(city));
-        let universities = UNIVERSITIES.filter(uni => uni.cityID === cityObj.id);
-
-        universities.forEach(uni => {
-            finishArray.push(PROGRAMMES.filter(prog => prog.universityID === uni.id))
-        })
-
-        finishArray = finishArray.flat(1);
-        appendCards(finishArray);
-    }
-    // Om stad och input är ifyllt - done
-    else if (textValue.length > 0 && country.length === 0 && city.length > 0){
-        let cityObj = CITIES.find(c => c.name.includes(city));
-        let universities = UNIVERSITIES.filter(uni => uni.cityID === cityObj.id);
-
-        let programs = [];
-        universities.forEach(uni => {
-            programs.push(PROGRAMMES.filter(prog => prog.universityID === uni.id))
-        })
-        
-        finishArray = programs.flat(1).filter(prog => prog.name.toLowerCase().includes(textValue));
-        appendCards(finishArray);
+        if(textValue != "--- Land ---"){
+            let programs = [];
+            universities.forEach(uni => {
+                programs.push(PROGRAMMES.filter(prog => prog.universityID === uni.id));
+                finishArrayFiltered = programs.flat(1).filter(prog => prog.name.toLowerCase().includes(textValue));
+                appendCards(finishArrayFiltered);
+            });
+        }
+        else{
+            let programs = [];
+            console.log(universities);
+            universities.forEach(uni => {
+                programs.push(PROGRAMMES.filter(prog => prog.universityID === uni.id))
+                finishArrayFiltered = programs.flat(1);
+            });
+            appendCards(finishArrayFiltered);
+        }  
     }
     // Om land och input är ifyllt - done 
-    else if (textValue.length > 0 && country.length > 0 && city.length === 0){
+    else if (country.length > 0){
         let countryObj = COUNTRIES.find(c => c.name.includes(country));
         let citys = CITIES.filter(ci => ci.countryID === countryObj.id);
     
         let universities = [];
         citys.forEach(city => {
             universities.push(UNIVERSITIES.filter(uni => uni.cityID === city.id));
-        })
-
-        let programs = [];
-        universities.flat(1).forEach(uni => {
-            programs.push(PROGRAMMES.filter(prog => prog.universityID === uni.id));
-        })
-
-        finishArray = programs.flat(1).filter(prog => prog.name.toLowerCase().includes(textValue));
-        appendCards(finishArray);
+        });
+        if(textValue.length > 0){
+            let programs = [];
+            universities.flat(1).forEach(uni => {
+                programs.push(PROGRAMMES.filter(prog => prog.universityID === uni.id));
+            });
+            finishArrayFiltered = programs.flat(1).filter(prog => prog.name.toLowerCase().includes(textValue));
+            appendCards(finishArrayFiltered);
+        }
+        else {
+            let programs = [];
+            universities.flat(1).forEach(uni => {
+                programs.push(PROGRAMMES.filter(prog => prog.universityID === uni.id));
+            });
+            finishArrayFiltered = programs.flat(1)
+            appendCards(finishArrayFiltered);
+        }
     }
-    // Om alla är ifyllda - NOT done
-    else if (textValue.length > 0 && country.length > 0 && city.length > 0){
-        console.log("alla ifyllda");
-    }
-
+    // Om alla är ifyllda - Klar
     function appendCards(array){
         array.forEach(program => {
             document.querySelector(".programList").append(createCard(program));
         })
     }
-
-     console.log(finishArray);
 }
 
 
